687ed6bdb737fbf88a5fc39fce88384ff14412f1
Chukwuebuka Valentine Okoye-Nobert <o.chukwuebu@alustudent.com>
2022-04-07 11:53:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,9 @@ def transform_text(text):
 tfidf = pickle.load(open('vectorizer.pkl','rb'))
 model = pickle.load(open('model.pkl','rb'))
 
-st.title("Email/SMS Spam Classifier")
+st.title("Fake and Geniune Review Detection")
+
+st.text('The Review system is a system that uses BiLSTM model to detect whether an inputted review is fake or genuine')
 
 input_sms = st.text_area("Enter the message")
 
